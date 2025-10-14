21205d8da77bb1170c9c5edbe5a0bd9e94ba644c
luis felipe <daluz0221@gmail.com>
2025-10-14 17:18:55 -0500

    new repo y adding github actions

---
+++ src/main/java/com/udea/lab2luis/Lab2luisApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2luis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2luisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2luisApplication.class, args);
+	}
+
+}
